d701719892e3c3ee815e9fe4f89395c65f43e00c
P . Sai Charan Kumar <87646166+charanccherry@users.noreply.github.com>
2021-09-24 22:27:15 +0530

    Create app.js

---
+++ App/app.js
@@ -0,0 +1,23 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import AboutJobItem from './components/AboutJobItem'
+import AllJobs from './components/AllJobs'
+import ProtectedRoute from './components/ProtectedRoute'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/jobs" component={AllJobs} />
+    <ProtectedRoute exact path="/jobs/:id" component={AboutJobItem} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
+  </Switch>
+)
+
+export default App
